9ebc18c919d52bc031fd824defddd04804f738c5
faineBenk <tinagreen@mail.ru>
2022-06-29 01:03:05 +0300

    url_availability added

---
+++ url_availability.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#https://linuxhint.com/read_file_line_by_line_bash/
+
+file=$1
+read "${file}"
+
+function check_urls() { 
+	while read -r url; do
+		echo $url
+		command=$(curl -sL -w "%{http_code}\\n" $url -o /dev/null)
+		if [ $command == "200" ]; then
+              		echo "http response is:" $command
+              		echo "SUCCESS"
+        	elif [[ $command == "000" ]] || [[ $command == "500" ]] || [[ $command == "400" ]]; then
+              		echo "FAILURE" $command
+              		exit 1
+        	else 
+              		echo "https response is:" $command
+        	fi
+	done < "$file"
+}
+
+check_urls 
+
